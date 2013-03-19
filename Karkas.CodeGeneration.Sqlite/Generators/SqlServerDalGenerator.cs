5c11f759f178a8a8e8d1c643a3eda355d5a475b1
Atilla Ozgur <ati.ozgur@gmail.com>
2013-03-19 17:04:34 +0200

    SqliteDalGenerator

---
+++ Karkas.CodeGeneration.Sqlite/Generators/SqlServerDalGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Karkas.CodeGenerationHelper.Generators;
+using Karkas.CodeGenerationHelper.Interfaces;
+
+namespace Karkas.CodeGeneration.Sqlite.Generators
+{
+    public class SqliteDalGenerator : DalGenerator
+    {
+        public SqliteDalGenerator(IDatabaseHelper databaseHelper)
+            : base(databaseHelper)
+        {
+        }
+
+        protected override string parameterSymbol
+        {
+            get { return "@"; }
+        }
+
+        protected override string getDbTargetType(IColumn column)
+        {
+            if (column.DbTargetType == "Unknown")
+            {
+                return "SqlDbType.VarChar";
+            }
+            else
+            {
+                return column.DbTargetType;
+            }
+        }
+
+
+    }
+}
