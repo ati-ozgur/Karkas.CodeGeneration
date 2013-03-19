19751e1a586318c1263905339821cdc970a9e20e
Atilla Ozgur <ati.ozgur@gmail.com>
2013-03-19 16:02:59 +0200

    sqlite ekleme calısmaları v1

---
+++ Karkas.CodeGeneration.Sqlite/SqliteHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Karkas.CodeGenerationHelper.Interfaces;
+using Karkas.Core.DataUtil;
+
+namespace Karkas.CodeGeneration.Sqlite
+{
+    public class SqliteHelper : IDatabaseHelper
+    {
+        public string getDatabaseName(AdoTemplate template)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getDefaultSchema(AdoTemplate template)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Data.DataTable getTableListFromSchema(AdoTemplate template, string schemaName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Data.DataTable getSchemaList(AdoTemplate template)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CodeGenerateAllTables(AdoTemplate template, string pConnectionString, string pDatabaseName, string pProjectNamespace, string pProjectFolder, bool dboSemaTablolariniAtla, bool sysTablolariniAtla, List<CodeGenerationHelper.DatabaseAbbreviations> listDatabaseAbbreviations)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CodeGenerateOneTable(AdoTemplate template, string pConnectionString, string pTableName, string pSchemaName, string pDatabaseName, string pProjectNamespace, string pProjectFolder, List<CodeGenerationHelper.DatabaseAbbreviations> listDatabaseAbbreviations)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CodeGenerationHelper.Generators.DalGenerator DalGenerator
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
