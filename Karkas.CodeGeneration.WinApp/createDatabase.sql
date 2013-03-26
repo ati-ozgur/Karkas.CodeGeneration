f5ae441d14e6fba753724e28d57cae367b2f0ae6
Atilla Ozgur <ati.ozgur@gmail.com>
2013-03-26 15:07:00 +0200

    database dosyasına örnek entry

--- Karkas.CodeGeneration.WinApp/createDatabase.sql
+++ Karkas.CodeGeneration.WinApp/createDatabase.sql
@@ -3,13 +3,29 @@ drop table if exists DatabaseEntry;
 CREATE TABLE IF NOT EXISTS DatabaseEntry(
 ConnectionName TEXT primary key 
 ,ConnectionDatabaseType INTEGER
-,AbbrevationsAsString TEXT,ConnectionString TEXT
+,AbbrevationsAsString TEXT
+,ConnectionString TEXT
 ,CodeGenerationDirectory TEXT
 ,CodeGenerationNamespace TEXT
 ,CreationTime TEXT
 ,LastAccessTime TEXT
 ,LastWriteTime TEXT
 );
+
+INSERT INTO DatabaseEntry VALUES
+(
+'Karkas.Ornek'
+,1 
+,''
+,'Integrated Security = SSPI; Persist Security Info=False;Initial Catalog=KARKAS_ORNEK;Data Source=localhost'
+,'D:\\projects\\karkas\\Karkas.Ornek'
+,'Karkas.Ornek'
+,''
+,''
+,''
+);
+
+
 COMMIT;
 
 
