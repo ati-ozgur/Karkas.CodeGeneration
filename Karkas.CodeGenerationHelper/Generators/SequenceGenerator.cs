9b5afb9efc3b9ba7d1ebbcb6279fb0f833370655
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-10 01:48:23 +0300

    sequence üretme v3

--- Karkas.CodeGenerationHelper/Generators/SequenceGenerator.cs
+++ Karkas.CodeGenerationHelper/Generators/SequenceGenerator.cs
@@ -52,6 +52,7 @@ namespace Karkas.CodeGenerationHelper.Generators
 
 
             SelectSequenceStringYaz(output, database, schemaName, sequenceName);
+            getNextSequenceValueYaz(output);
             BitisSusluParentezVeTabAzalt(output);
             BitisSusluParentezVeTabAzalt(output);
 
@@ -62,9 +63,30 @@ namespace Karkas.CodeGenerationHelper.Generators
 
         }
 
-        private void SelectSequenceStringYaz(IOutput output, IDatabase database, string schemaName, string sequenceName)
+        private void getNextSequenceValueYaz(IOutput output)
         {
             output.autoTabLn("");
+            output.autoTabLn("public decimal getNextSequenceValue()");
+            BaslangicSusluParentezVeTabArtir(output);
+            output.autoTabLn("AdoTemplate template = new AdoTemplate(DbProviderName);");
+            output.autoTabLn("return (decimal) template.TekDegerGetir(selectSequenceString);");
+            BitisSusluParentezVeTabAzalt(output);
+        }
+
+        private void SelectSequenceStringYaz(IOutput output, IDatabase database, string schemaName, string sequenceName)
+        {
+            string selectSequenceString = "";
+            if (database.UseSchemaNameInSqlQueries)
+            {
+                selectSequenceString = string.Format("private const string selectSequenceString = \"SELECT {0}.{1}.NEXTVAL FROM DUAL;\";", schemaName, sequenceName);
+            }
+            else
+            {
+                selectSequenceString = string.Format("private const string selectSequenceString = \"SELECT {0}.NEXTVAL FROM DUAL;\";",  sequenceName);
+            }
+            output.autoTabLn(selectSequenceString);
+
+
         }
 
         private void UsingleriYaz(IOutput output, string baseNameSpaceSequencesDal)
@@ -96,7 +118,7 @@ namespace Karkas.CodeGenerationHelper.Generators
 
         private void OverrideDbProviderNameYaz(IOutput output)
         {
-            output.autoTabLn("public override string DbProviderName");
+            output.autoTabLn("public string DbProviderName");
             BaslangicSusluParentezVeTabArtir(output);
             output.autoTabLn("get");
             BaslangicSusluParentezVeTabArtir(output);
