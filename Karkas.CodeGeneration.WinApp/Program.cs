b718a03c7ba53f12e1b0a90bb63d8d296f33601c
Atilla Ozgur <ati.ozgur@gmail.com>
2013-05-27 00:35:52 +0300

    2012 version added

--- Karkas.CodeGeneration.WinApp/Program.cs
+++ Karkas.CodeGeneration.WinApp/Program.cs
@@ -2,20 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using log4net;
 
 namespace Karkas.CodeGeneration.WinApp
 {
     static class Program
     {
+
+        static ILog logger = LogManager.GetLogger(typeof(Program));
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+
+            log4net.Config.XmlConfigurator.Configure();
+            logger.Info("Starting Application");
+
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += currentDomain_UnhandledException;
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        static void currentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            logger.Error(e);
+            if (e.IsTerminating)
+            {
+                logger.Error("terminate - Atilla");
+            }
         }
     }
 }
