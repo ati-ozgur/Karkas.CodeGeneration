49cb58913379eba18e344ad0c1e278182c3c315e
Atilla Ozgur <ati.ozgur@gmail.com>
2013-08-04 16:59:40 +0300

    Kullanılmayan using

--- Karkas.CodeGeneration.SqlServer/Implementations/TableSqlServer.cs
+++ Karkas.CodeGeneration.SqlServer/Implementations/TableSqlServer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Karkas.CodeGenerationHelper.Interfaces;
-using Microsoft.SqlServer.Management.Smo;
 using System.Data;
 using Karkas.Core.DataUtil;
 
